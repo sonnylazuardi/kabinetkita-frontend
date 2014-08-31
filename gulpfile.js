5e31410d3a3f515f48d89f93e08c2c3f3c69c0c3
Sonny Lazuardi <sonnylazuardi@gmail.com>
2014-08-31 12:52:01 +0700

    kandidat page

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp'),
 	less = require('gulp-less'),
+	minifyCSS = require('gulp-minify-css'),
     path = require('path');
 
 gulp.task('build-less', function(){
     return gulp.src('./less/bootstrap.less')
         .pipe(less())
+        .pipe(minifyCSS())
         .pipe(gulp.dest('./css'));
 });
 
